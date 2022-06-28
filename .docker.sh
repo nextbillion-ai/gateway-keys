1e111dcec82251ae6430950702c190b5131b99d9
lifu <lifu@nextbillion.ai>
2022-06-28 15:43:05 +0800

    fix: push to gcp ar also

--- .docker.sh
+++ .docker.sh
@@ -1,8 +1,11 @@
 set -e
 docker_host=nextbillionai
+docker_host_ar=asia-docker.pkg.dev/nextbillion/internal
 image_name=gateway-keys
 
 version=$CI_COMMIT_TAG
 docker build -t $docker_host/$image_name:$version .
 docker push $docker_host/$image_name:$version
+docker tag $docker_host/$image_name:$version $docker_host_ar/$image_name:$version
+docker push $docker_host_ar/$image_name:$version
 exit 0
